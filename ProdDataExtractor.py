c4c1e95311d96439f46b1a4b36ce986f1c24ade0
brincktron <brincktron@gmail.com>
2018-08-12 22:50:22 +0200

    small improvements

--- ProdDataExtractor.py
+++ ProdDataExtractor.py
@@ -21,11 +21,11 @@ filepaths = ['CBDFull[DutMic0]',
              'CBDMF[DutMic2]',
              ]
 full_path = 'et-data/CBDHF[DutMic0]/CBDHF[DutMic0] 2018-08-01.txt'
-relative_path = 'et-data/'
+relative_path = 'et-data'
 filenames = []
 x = 0
 while x < len(filepaths):
-    filenames.append(os.listdir(relative_path + filepaths[x]))
+    filenames.append(os.listdir(relative_path + '/'+ filepaths[x]))
     x += 1
 
 # with csv reader arrays
@@ -40,20 +40,40 @@ def csv_read_data(path):
             serial.append(data[0])
             value.append(data[-1])
     return serial, value
+
+
+# with pandas
+finalDf = pd.DataFrame()
+def panda_read_data(path, heading):
+    pandatable = pd.read_table(path)
+    serial = pandatable.Serialnr
+    value  =pandatable[heading]
+    return serial, value
+
 serial = []
 complete_data = {}
 x = 0
+
+#with pandas
+finalDf = pd.DataFrame()
 while x < len(filepaths):
     y = 0
     while y < len(filenames[x]):
-        complete_data[filepaths[x] + 'serial'], complete_data[filepaths[x]] = csv_read_data(relative_path + '/' + filepaths[x] + '/' + filenames[x][y])
+        full_path = relative_path + '/' + filepaths[x] + '/' + filenames[x][y]
+        # with pandas
+        pandaserial, finalDf[filepaths[x]] = panda_read_data(full_path, filepaths[x])
+        if y == 0:
+            complete_data[filepaths[x] + 'serial'], complete_data[filepaths[x]] = csv_read_data(full_path)
+        else:
+            complete_data[filepaths[x] + 'serial'].extend(csv_read_data(full_path)[0])
+            complete_data[filepaths[x]].extend(csv_read_data(full_path)[1])
         y += 1
     x += 1
 serial, value = csv_read_data(relative_path + '/' + filepaths[1] + '/' + filenames[1][0])
 
+# check serial numbers
+#for x in complete_data
+#    if complete_data[filepaths[x]] ==
 #with numpy arrays
 
-
-# with pandas
-pandatable = pd.read_table(full_path)
-table1 = pandatable.Serialnr
+print hej
