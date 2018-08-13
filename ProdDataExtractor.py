e6382c3cc4bc6acb3417e7ce5917b736960f3a95
brincktron <brincktron@gmail.com>
2018-08-13 23:11:41 +0200

    cleaning up and only using pandas

--- ProdDataExtractor.py
+++ ProdDataExtractor.py
@@ -20,63 +20,36 @@ filepaths = ['CBDFull[DutMic0]',
              'CBDMF[DutMic1-DutMic2]',
              'CBDMF[DutMic2]',
              ]
-full_path = 'et-data/CBDHF[DutMic0]/CBDHF[DutMic0] 2018-08-01.txt'
 relative_path = 'et-data'
 
 # create all paths
 filenames = []
-for x in filepaths:
+full_path = []
+x = 0
+while x < len(filepaths):
     filenames.append(os.listdir(relative_path + '/' + filepaths[x]))
-
-# with csv reader arrays
-def csv_read_data(path):
-    value = []
-    serial = []
-    with open(path, 'r') as f:
-        next(f) # skip headings
-        reader = csv.reader(f, delimiter='\t')
-        # the data we are interested in are the first (serial) and the last (CBD data) value
-        for data in reader:
-            serial.append(data[0])
-            value.append(data[-1])
-    return serial, value
-
+    y = 0
+    while y < len(filenames[x]):
+        full_path[x] = relative_path + '/' + filepaths[x] + '/' + filenames[x][y]
+    x += 1
 
 # with pandas
 finalDf = pd.DataFrame()
 def panda_read_data(path, heading):
     pandatable = pd.read_table(path)
-    serial = pandatable.Serialnr
     value  =pandatable[heading]
-    return serial, value
+    return value
 
-serial = []
-complete_data = {}
 x = 0
 y = 0
-filenames = []
 #with pandas
 finalDf = pd.DataFrame()
 while x < len(filepaths):
     y = 0
-    while y < len(filenames[x]):
-        full_path = relative_path + '/' + filepaths[x] + '/' + filenames[x][y]
-        print y
-        if y == 0:
-            complete_data[filepaths[x] + 'serial'], complete_data[filepaths[x]] = csv_read_data(full_path)
-            pandaserial, finalDf[filepaths[x]] = panda_read_data(full_path, filepaths[x])
-        else:
-            complete_data[filepaths[x] + 'serial'].extend(csv_read_data(full_path)[0])
-            complete_data[filepaths[x]].extend(csv_read_data(full_path)[1])
-            pandaserial.append(panda_read_data(full_path, filepaths[x])[0])
-            finalDf[filepaths[x]].append(panda_read_data(full_path, filepaths[x])[1])
-        y += 1
+    if y == 0:
+        finalDf[filepaths[x]] = panda_read_data(full_path[x], filepaths[x])
+    else:
+        finalDf[filepaths[x]].append(panda_read_data(full_path, filepaths[x]))
     x += 1
-serial, value = csv_read_data(relative_path + '/' + filepaths[1] + '/' + filenames[1][0])
-
-# check serial numbers
-#for x in complete_data
-#    if complete_data[filepaths[x]] ==
-#with numpy arrays
 
 print hej
